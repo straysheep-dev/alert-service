ac70223713ab1df3ed038f9eece2174a1047f361
straysheep-dev <84547874+straysheep-dev@users.noreply.github.com>
2023-10-22 21:29:27 -0700

    Update RegisterTask-LoginAlert.ps1

--- RegisterTask-LoginAlert.ps1
+++ RegisterTask-LoginAlert.ps1
@@ -1,3 +1,5 @@
+# RegisterTask-LoginAlert.ps1
+
 $taskname = "Login Alert"
 $action = New-ScheduledTaskAction -Execute "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Argument "-nop -ep bypass -w hidden C:\Tools\Scripts\Send-LoginAlert.ps1"
 $trigger = New-ScheduledTaskTrigger -AtLogon
